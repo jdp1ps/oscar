fc1ad16010c5e1523d8d53d289b45900364b1c15
Stephane Bouvry <stephane.bouvry@unicaen.fr>
2017-05-17 11:02:04 +0200

    Script de purge des données pour le DEV/Installation

---
+++ bin/dev-db-purge-datas.sql
@@ -0,0 +1,25 @@
+﻿-- Suppression des jointures
+DELETE FROM projectmember;
+DELETE FROM projectpartner;
+DELETE FROM activityperson;
+DELETE FROM activityorganization;
+DELETE FROM activitydate;
+DELETE FROM activitypayment;
+DELETE FROM contractdocument;
+DELETE FROM organizationperson;
+DELETE FROM workpackageperson;
+
+-- Suppression des données
+DELETE FROM timesheet;
+DELETE FROM workpackage;
+DELETE FROM activity;
+DELETE FROM logactivity;
+DELETE FROM activity;
+DELETE FROM person;
+DELETE FROM organization;
+DELETE FROM authentification;
+
+
+
+
+
