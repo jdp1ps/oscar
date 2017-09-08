b76b4a3d04be75dfd32f8af85917a706714920bc
bouvry <stephane.bouvry@unicaen.fr>
2017-09-08 13:35:34 +0200

    Fix #94 et ammélioration des résultats sur les filtres

--- config/application.config.php
+++ config/application.config.php
@@ -7,6 +7,7 @@ $config = array(
         'ZfcUser',
         'ZfcUserDoctrineORM',
         'BjyAuthorize',
+        //'ZendDeveloperTools',
         'UnicaenApp',
         'UnicaenAuth',
         'Oscar',
@@ -24,7 +25,7 @@ $config = array(
 );
 
 if( getenv('APPLICATION_ENV') == 'development' ){
-//    $config['modules'][] = 'ZendDeveloperTools';
+    //$config['modules'][] = 'ZendDeveloperTools';
 }
 
 
