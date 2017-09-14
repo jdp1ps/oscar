8c337fce38b11f53fdc73079d6793e2dfff38b0c
bouvry <stephane.bouvry@unicaen.fr>
2017-09-14 15:18:14 +0200

    Test connexion OK, début affichage AJAX

--- module/Oscar/src/Oscar/Service/NotificationService.php
+++ module/Oscar/src/Oscar/Service/NotificationService.php
@@ -7,6 +7,7 @@
 
 namespace Oscar\Service;
 
+use Oscar\Entity\Notification;
 use UnicaenApp\Service\EntityManagerAwareInterface;
 use UnicaenApp\Service\EntityManagerAwareTrait;
 use Zend\ServiceManager\ServiceLocatorAwareInterface;
@@ -17,7 +18,52 @@ class NotificationService implements ServiceLocatorAwareInterface, EntityManager
     use ServiceLocatorAwareTrait, EntityManagerAwareTrait;
 
     public function getNotificationsPerson( $personId ){
-        $notifications = [];
-        return $notifications;
+        $result = [
+            'personid' => $personId,
+            'notifications' => []
+        ];
+
+        $notifications = $this->getEntityManager()->getRepository(Notification::class)->findBy([
+            'recipientId' => $personId
+        ]);
+
+        /** @var Notification $notification */
+        foreach ($notifications as $notification ){
+            $result['notifications'][] = $notification->toArray();
+        }
+
+        return $result;
+    }
+
+    public function notification( $message, $personsId, $key=null ){
+        if( $key === null ){
+            $key = uniqid();
+        }
+        $push = [];
+        foreach ( $personsId as $personid ){
+            echo "Envoi à $personid\n";
+            $hash = md5($personid.'/'.$key);
+            $date = new \DateTime();
+            $notification = new Notification();
+            $notification->setContext('Application')
+                ->setContextId(-1)
+                ->setDatas([])
+                ->setMessage($message)
+                ->setDateEffective($date)
+                ->setLevel(Notification::LEVEL_INFO)
+                ->setRead(false)
+                ->setRecipientId($personid);
+            $this->getEntityManager()->persist($notification);
+
+            $this->getEntityManager()->flush($notification);
+            $push[] = $notification->getId();
+        }
+
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "http://localhost:3000/push");
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, "ids=".implode(',', $push));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_exec($curl);
     }
 }
