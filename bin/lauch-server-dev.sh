7173d861ec9f96a93462755705941e303d20ce9b
bouvry <stephane.bouvry@unicaen.fr>
2017-11-07 11:23:22 +0100

    Fix composer upgrade

--- bin/lauch-server-dev.sh
+++ bin/lauch-server-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # A exécuter depuis le dossier développement
 PORT=4000
-APPLICATION_ENV=production php -S 127.0.0.1:$PORT -t public public/index.php --env=dev
+APPLICATION_ENV=production php -S 127.0.0.1:$PORT -t public public/index.php
