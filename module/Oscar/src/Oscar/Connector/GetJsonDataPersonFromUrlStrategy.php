671cd82dc4f5c9fd8a92d4b7506b56a5bc1b6194
bouvry <stephane.bouvry@unicaen.fr>
2017-11-16 18:04:27 +0100

    Mise au propre / centralisation des traitements / tests unitaires pour les connectors

---
+++ module/Oscar/src/Oscar/Connector/GetJsonDataPersonFromUrlStrategy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Stéphane Bouvry<stephane.bouvry@unicaen.fr>
+ * @date: 17-11-16 17:43
+ * @copyright Certic (c) 2017
+ */
+
+namespace Oscar\Connector;
+
+
+class GetJsonDataPersonFromUrlStrategy implements GetJsonDataStrategy
+{
+
+    function getDataJson()
+    {
+        // TODO: Implement getDataJson() method.
+    }
+}
