1597214787e3c0de55c0647b2a98982f424d4c18
bouvry <stephane.bouvry@unicaen.fr>
2017-11-17 10:56:10 +0100

    Test OK sur la stratégie via un fichier

---
+++ module/Oscar/src/Oscar/Connector/GetJsonDataStrategyTrait.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Stéphane Bouvry<stephane.bouvry@unicaen.fr>
+ * @date: 17-11-17 10:48
+ * @copyright Certic (c) 2017
+ */
+
+namespace Oscar\Connector;
+
+
+class GetJsonDataStrategyTrait
+{
+
+}
