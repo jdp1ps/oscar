917d133d5b68ed7b630610a5b25d329d7baae434
bouvry <stephane.bouvry@unicaen.fr>
2017-11-17 10:47:31 +0100

    Test OK sur la stratégie via un fichier

--- tests/Oscar/Factory/JsonToOrganizationFactoryTest.php
+++ tests/Oscar/Factory/JsonToOrganizationFactoryTest.php
@@ -19,7 +19,8 @@ class JsonToOrganizationFactoryTest extends TestCase {
     public function testGetInstance(){
 
         try {
-            $organization = $this->getFactory()->getInstance((object) null, 'test');
+            $organization = $this->getFactory()->getInstance((object) null,
+                'GetJsonDataFromFileStrategyTest');
             throw PHPUnitException('Un objet sans les champs requis doit lever une exception');
         } catch (\Oscar\Exception\OscarException $e ){
             $this->assertTrue(True);
@@ -28,10 +29,11 @@ class JsonToOrganizationFactoryTest extends TestCase {
 
         $organization = $this->getFactory()->getInstance((object) [
             'code' => 'ED209',
-            'shortname' => 'Etude Directive 20 Al 9'], 'test');
+            'shortname' => 'Etude Directive 20 Al 9'],
+            'GetJsonDataFromFileStrategyTest');
 
         $this->assertEquals('Etude Directive 20 Al 9', $organization->getShortName());
-        $this->assertEquals('ED209', $organization->getConnectorID('test'));
+        $this->assertEquals('ED209', $organization->getConnectorID('GetJsonDataFromFileStrategyTest'));
         $this->assertEquals('ED209', $organization->getCode());
         $this->assertNull( $organization->getConnectorID('unknown'), "Getter sur un connector absent retourne NULL");
     }
