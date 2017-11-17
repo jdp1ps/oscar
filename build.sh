50556fbf54362085304d9381d1520dc9b8c1aa99
bouvry <stephane.bouvry@unicaen.fr>
2017-11-17 17:46:36 +0100

    Save de la conf pour les test dans Docker

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo "TOTO"
+
+# Création de l'utilisateur POSTGRESQL pour les Tests
+service postgresql start
+
+sudo -u postgres psql -c "CREATE USER oscar_test WITH PASSWORD 'azerty';"
+sudo -u postgres psql -c "CREATE DATABASE oscar_test;"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE oscar_test TO oscar_test"
+
+sudo -u postgres psql oscar_test < install/oscar-install.sql
+
+php vendor/bin/doctrine-module orm:schema-tools:update --force
