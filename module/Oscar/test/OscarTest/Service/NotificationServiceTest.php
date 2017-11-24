1d3dd1f6b48b5a38dad3d321be15f9422c2d90d2
bouvry <stephane.bouvry@unicaen.fr>
2017-11-24 14:57:51 +0100

    FIX #173 Suppression des clefs de configuration en double

---
+++ module/Oscar/test/OscarTest/Service/NotificationServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Stéphane Bouvry<stephane.bouvry@unicaen.fr>
+ * @date: 17-11-24 11:36
+ * @copyright Certic (c) 2017
+ */
+
+namespace OscarTest\Service;
+
+
+class NotificationServiceTest
+{
+
+}
