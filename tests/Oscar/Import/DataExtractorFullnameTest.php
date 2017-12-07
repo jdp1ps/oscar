5d16123ef4d819a2e0039220fef7ad9d6f8be599
jacksay <jacksay@jacksay.com>
2017-12-07 11:30:19 +0100

    Ajout de l'email facultatif dans les emails et extraction des CODE / Nom court / Nom long dans les organisations.

--- tests/Oscar/Import/DataExtractorFullnameTest.php
+++ tests/Oscar/Import/DataExtractorFullnameTest.php
@@ -20,9 +20,34 @@ class DataExtractorFullnameTest extends TestCase
         $out = $extractor->extract("Stéphane Bouvry");
         $this->assertEquals("Stéphane", $out['firstname']);
         $this->assertEquals("Bouvry", $out['lastname']);
+        $this->assertEquals("", $out['email']);
+    }
+
+
+    public function testCommatFirst(){
+        $extractor = new DataExtractorFullname();
 
         $out = $extractor->extract("Jean-Claude Dus");
         $this->assertEquals("Jean-Claude", $out['firstname']);
         $this->assertEquals("Dus", $out['lastname']);
+        $this->assertEquals("", $out['email']);
+    }
+
+    public function testCommatLastValid(){
+        $extractor = new DataExtractorFullname();
+
+        $out = $extractor->extract("Stéphane Pierre-Henry");
+        $this->assertEquals("Stéphane", $out['firstname']);
+        $this->assertEquals("Pierre-Henry", $out['lastname']);
+        $this->assertEquals("", $out['email']);
+    }
+
+    public function testMailLastValid(){
+        $extractor = new DataExtractorFullname();
+
+        $out = $extractor->extract("Stéphane Brochet <stephane.brochet@email.com>");
+        $this->assertEquals("Stéphane", $out['firstname']);
+        $this->assertEquals("Brochet", $out['lastname']);
+        $this->assertEquals("stephane.brochet@email.com", $out['email']);
     }
 }
