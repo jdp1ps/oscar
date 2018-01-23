86c0674c6567447aba034aa9cb0e80dab969a53d
bouvry <stephane.bouvry@unicaen.fr>
2018-01-23 15:23:47 +0100

    Suppression du var_dump de l'enfer

--- module/Oscar/src/Oscar/Controller/TimesheetController.php
+++ module/Oscar/src/Oscar/Controller/TimesheetController.php
@@ -81,7 +81,6 @@ class TimesheetController extends AbstractOscarController
 
             /** @var Activity $activity */
             $activity = $this->getEntityManager()->getRepository(Activity::class)->find($activityId);
-           var_dump($datas[$activityId]['timesheets']);
 
             $cellDays = ['C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U', 'V', 'W','X','Y','Z','AA', 'AB', 'AC', 'AD', 'AE','AF','AG'];
             $lineWpFormula = '=SUM(C%s:AG%s)';
