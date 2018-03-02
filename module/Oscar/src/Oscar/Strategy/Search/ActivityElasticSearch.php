14c9de72d9d999c99ac36887a0aaf1c4ab558509
bouvry <stephane.bouvry@unicaen.fr>
2018-03-02 11:43:18 +0100

    Module de recherche ElasicSearch v1.0

--- module/Oscar/src/Oscar/Strategy/Search/ActivityElasticSearch.php
+++ module/Oscar/src/Oscar/Strategy/Search/ActivityElasticSearch.php
@@ -15,33 +15,124 @@ use Elasticsearch\ClientBuilder;
 class ActivityElasticSearch implements ActivitySearchStrategy
 {
     private $elasticSearchClient;
+    private $hosts;
+    private $index = 'oscar';
+    private $type = 'activity';
 
+    /**
+     * ActivityElasticSearch constructor.
+     * @param $hosts
+     */
+    public function __construct($hosts)
+    {
+        $this->hosts = $hosts;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndex(): string
+    {
+        return $this->index;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function getHosts(){
+        return $this->hosts;
+    }
 
     public function search($search)
     {
-        // TODO: Implement search() method.
+        $params = [
+            'index' => $this->getIndex(),
+            'type' => $this->getType(),
+            'body' => [
+                'query' => [
+                    'query_string' => [
+                        'query' => $search
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $this->getClient()->search($params);
+        $ids = [];
+        if( $response && $response['hits'] && $response['hits']['total'] > 0 ){
+            foreach ($response['hits']['hits'] as $hit) {
+                $ids[] = $hit["_id"];
+            }
+        }
+        return $ids;
     }
 
     protected function getClient()
     {
         if( !$this->elasticSearchClient )
-            $this->elasticSearchClient = ClientBuilder::create()->build();
+            $this->elasticSearchClient = ClientBuilder::create()
+                ->setHosts($this->getHosts())
+                ->build();
         return $this->elasticSearchClient;
     }
 
     public function addActivity(Activity $activity)
     {
-        $params = [
-            'index' => 'activity',
-            'type' => 'activity',
-            'id' => $activity->getId(),
-            'body' => [
-                'label' => $activity->getLabel(),
-                'acronym' => $activity->getAcronym(),
+
+
+        $params = ['body' => []];
+
+        $params['body'][] = [
+            'index' => [
+                '_index' => $this->getIndex(),
+                '_type' => $this->getType(),
+                '_id' => $activity->getId(),
             ]
         ];
 
-        $response = $this->getClient()->index($params);
+        $params['body'][] = $this->getIndexableDatas($activity);
+
+        return $this->getClient()->bulk($params);
+    }
+
+    protected function getIndexableDatas( Activity $activity ){
+        $project_body = "";
+        $project_id = null;
+
+        if( $activity->getProject() ){
+            $project_body = $activity->getProject()->getCorpus();
+            $project_id = $activity->getProject()->getId();
+        }
+
+        $members = [];
+        $partners = [];
+
+        foreach ( $activity->getPersonsDeep() as $personRoled ){
+            $members[] = (string) $personRoled->getPerson();
+        }
+
+        foreach ( $activity->getOrganizationsDeep() as $organizationRolead ){
+            $partners[] = (string) $organizationRolead->getOrganization();
+        }
+
+        return [
+            'label' => $activity->getLabel(),
+            'description' => $activity->getDescription(),
+            'saic' => $activity->getCentaureId(),
+            'oscar' => $activity->getOscarId(),
+            'eotp' => $activity->getCodeEOTP(),
+            'acronym' => $activity->getAcronym(),
+            'activity_id' => $activity->getId(),
+            'members' => $members,
+            'partners' => $partners,
+            'project_id' => $project_id,
+            'project' => $project_body,
+        ];
     }
 
     public function searchProject($what)
@@ -51,16 +142,30 @@ class ActivityElasticSearch implements ActivitySearchStrategy
 
     public function searchDelete($id)
     {
-        // TODO: Implement searchDelete() method.
+        return $this->getClient()->delete([
+            'index' => $this->getIndex(),
+            'type' => $this->getType(),
+            'id' => $id
+        ]);
     }
 
     public function searchUpdate(Activity $activity)
     {
-        // TODO: Implement searchUpdate() method.
+        $params = [
+            'index' => $this->getIndex(),
+            'type' => $this->getType(),
+            'id' => $activity->getId(),
+            'body' => $this->getIndexableDatas()
+        ];
+        return $this->getClient()->update($params);
     }
 
     public function resetIndex()
     {
-        // TODO: Implement resetIndex() method.
+        $params = [
+            'index' => 'oscar'
+        ];
+
+        $this->getClient()->indices()->delete($params);
     }
 }
--- module/Oscar/src/Oscar/Strategy/Search/ActivitySearchStrategy.php
+++ module/Oscar/src/Oscar/Strategy/Search/ActivitySearchStrategy.php
@@ -13,10 +13,50 @@ use Oscar\Entity\Activity;
 
 interface ActivitySearchStrategy
 {
+    /**
+     * Lance la recherche à partir de la chaîne de recherche, retourne une liste d'ID.
+     *
+     * @param $search
+     * @return mixed
+     */
     public function search( $search );
+
+    /**
+     * Ajoute une activité au moteur de recherche.
+     *
+     * @param Activity $activity
+     * @return mixed
+     */
     public function addActivity(Activity $activity);
+
+    /**
+     * Recherche dans les projets.
+     *
+     * @param $what
+     * @return mixed
+     */
     public function searchProject($what);
+
+    /**
+     * Supprime l'activité du moteur de recherche.
+     *
+     * @param $id
+     * @return mixed
+     */
     public function searchDelete( $id );
+
+    /**
+     * Mise à jour de l'activité dans le moteur de recherche.
+     *
+     * @param Activity $activity
+     * @return mixed
+     */
     public function searchUpdate( Activity $activity );
+
+    /**
+     * Supprime l'index du moteur de recherche.
+     *
+     * @return mixed
+     */
     public function resetIndex();
 }
