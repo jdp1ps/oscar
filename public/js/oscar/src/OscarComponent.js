566961fb1aee702ed94c76b7d75cfe5154ff0cc4
bouvry <stephane.bouvry@unicaen.fr>
2019-03-13 16:27:31 +0100

    Mixins composant

--- public/js/oscar/src/OscarComponent.js
+++ public/js/oscar/src/OscarComponent.js
@@ -1,30 +1,16 @@
-<template>
-    <div>
-        <div class="overlay">
-            <div class="overlay-content" style="">
-                {{ loadingMsg }}
-                <div class="overlay-closer" @click="loading = false">X</div>
-            </div>
-        </div>
-    </div>
-</template>
-<script>
-
-    // nodejs node_modules/.bin/poi watch --format umd --moduleName  ActivityPersons --filename.js ActivityPersons.js --dist public/js/oscar/dist public/js/oscar/src/ActivityPersons.vue
 
     export default {
         props: {
-            url: { required: true }
-        },
-
-        data(){
-            return {
-                error: "",
-                loading: ""
-            }
+            url: { required: true },
+            urlPost: { required: true },
+            urlDelete: { required: true },
+            urlEdit: {required: true }
         },
 
         methods:{
+
+
+
             fetch(onOk){
                 this.loading = this.label +" : chargement...";
                 this.$http.get(this.url).then(
@@ -43,5 +29,4 @@
                 });
             }
         },
-    }
-</script>
+    };
