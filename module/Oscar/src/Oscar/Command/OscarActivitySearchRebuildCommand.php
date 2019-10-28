a299b471b2bfe4c7df33e1cfb3b240dc862093da
Jack <jacksay14@gmail.com>
2019-10-28 13:07:00 +0100

    Ajout de la commande de reconstruction d'index de recherche des activités

---
+++ module/Oscar/src/Oscar/Command/OscarActivitySearchRebuildCommand.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bouvry
+ * Date: 04/10/19
+ * Time: 11:49
+ */
+
+namespace Oscar\Command;
+
+
+use Moment\Moment;
+use Oscar\Entity\Authentification;
+use Oscar\Entity\LogActivity;
+use Oscar\Entity\Person;
+use Oscar\Entity\Role;
+use Oscar\Service\ConnectorService;
+use Oscar\Service\OrganizationService;
+use Oscar\Service\OscarConfigurationService;
+use Oscar\Service\OscarUserContext;
+use Oscar\Service\PersonService;
+use Oscar\Service\ProjectGrantService;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class OscarActivitySearchRebuildCommand extends OscarCommandAbstract
+{
+    protected static $defaultName = 'oscar:activity:search-rebuild';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription("Reconstruction de l'index de recherche des activités")
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->addOutputStyle($output);
+
+        /** @var OscarUserContext $oscaruserContext */
+        $oscaruserContext = $this->getServicemanager()->get(OscarUserContext::class);
+
+        $io = new SymfonyStyle($input, $output);
+
+        $io->title("Reconstruction de l'index de recherche des activités");
+
+        /** @var OscarConfigurationService $oscarConfig */
+        $oscarConfig = $this->getServicemanager()->get(OscarConfigurationService::class);
+
+        /** @var ProjectGrantService $projectGrantService */
+        $projectGrantService = $this->getServicemanager()->get(ProjectGrantService::class);
+
+        try {
+           $projectGrantService->searchIndex_rebuild();
+
+        } catch (\Exception $e ){
+            $io->error($e->getMessage() . "\n" . $e->getTraceAsString());
+        }
+    }
+}
