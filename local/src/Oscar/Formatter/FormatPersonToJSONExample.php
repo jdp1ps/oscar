65cd26fc67e50e612145acd5c67a2b37ad4f4751
Jack <jacksay14@gmail.com>
2020-01-21 17:53:52 +0100

    Exemple de formatteur JSON (en cours)

--- local/src/Oscar/Formatter/FormatPersonToJSONExample.php
+++ local/src/Oscar/Formatter/FormatPersonToJSONExample.php
@@ -6,7 +6,48 @@
  * Time: 18:11
  */
 
-class FormatPersonToJSONExample
-{
+namespace Oscar\Formatter;
 
+use Oscar\Entity\Person;
+
+class FormatPersonToJSONExample extends PersonToJsonConnectorFormatter
+{
+    /**
+     * Retourne le rôle correspondant.
+     *
+     * @param $roleRecu
+     * @return mixed|null
+     */
+    private static function getCorrespondance( $roleRecu ){
+        static $correspondances;
+        if( $correspondances === null ){
+            $correspondances = [
+                "Directeur de laboratoire" => "Directeur administratif de composante ",
+                "Directeur de composante" => "Directeur administratif de composante "
+            ];
+        }
+        if( array_key_exists($roleRecu, $correspondances) ){
+            return $correspondances[$roleRecu];
+        } else {
+            return null;
+        }
+    }
+
+    public function format(Person $person)
+    {
+        $output = parent::format($person);
+        $rolesRecus = $output['roles'];
+        $rolesReels = [];
+        foreach ($rolesRecus as $codeStructure => $roles) {
+            $rolesStructure = [];
+            foreach ($roles as $roleRecu ){
+                if( null != ($roleReel = $this->getCorrespondance($roleRecu)) ){
+                    $rolesStructure[] = $roleReel;
+                }
+            }
+            $rolesReels[$codeStructure] = $rolesStructure;
+        }
+        $output['roles'] = $rolesReels;
+        return $output;
+    }
 }
