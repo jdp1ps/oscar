1d5f79d2b6558c125e3d1baa41a9f0b7d8cbc46d
Stéphane Bouvry <stephane.bouvry@unicaen.fr>
2020-09-16 12:15:25 +0200

    FIX : Format de donnée monnétaire dans l'export Excel

--- module/Oscar/src/Oscar/Formatter/CSVDownloader.php
+++ module/Oscar/src/Oscar/Formatter/CSVDownloader.php
@@ -9,11 +9,14 @@
 namespace Oscar\Formatter;
 
 
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
+
 class CSVDownloader
 {
 
-    public function downloadCSVToExcel($csvPath){
-        $xlsPath = $csvPath.'.xls';
+    public function downloadCSVToExcel($csvPath)
+    {
+        $xlsPath = $csvPath . '.xls';
 
         $doc = new \PHPExcel();
 
@@ -25,26 +28,29 @@ class CSVDownloader
         $row = 1;
         if (($handle = fopen($csvPath, "r")) !== FALSE) {
             $cell = 1;
-            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, "\t")) !== FALSE) {
 
                 $num = count($data);
-                for ($c=0; $c < $num; $c++) {
+                for ($c = 0; $c < $num; $c++) {
                     $value = $data[$c];
-                    if(preg_match('/[0-9]*,[0-9]*/', $value)){
-                       $sheet->setCellValueExplicitByColumnAndRow($c, $row, $value, \PHPExcel_Cell_DataType::TYPE_NUMERIC);
-                    }
 
-                    elseif (preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2}/', $value)) {
-                       $sheet->setCellValueExplicitByColumnAndRow($c, $row, $value, \PHPExcel_Cell_DataType::TYPE_STRING);
-                    }
-
-                    else {
+                    if (preg_match('/([0-9 ]*),([0-9]{2})/', $value)) {
+                        //echo "Value reçue : $value\n";
+                        // Il faut convertir en "vrai" nombre
+                        $value = str_replace(',', '.', $value);
+                        //echo "Convertie : $value\n";
+                        $cell = $sheet->setCellValueExplicitByColumnAndRow($c, $row, $value, \PHPExcel_Cell_DataType::TYPE_NUMERIC, true);
+                        $cell->getStyle()->getNumberFormat()->setFormatCode(NumberFormat::FORMAT_CURRENCY_EUR);
+                    } elseif (preg_match('/[0-9]{4}-[0-9]{2}-[0-9]{2}/', $value)) {
+                        $sheet->setCellValueExplicitByColumnAndRow($c, $row, $value, \PHPExcel_Cell_DataType::TYPE_STRING);
+                    } else {
                         $sheet->setCellValueExplicitByColumnAndRow($c, $row, $value, \PHPExcel_Cell_DataType::TYPE_STRING);
                     }
                 }
                 $row++;
             }
             fclose($handle);
+            //die();
         }
         $writer = \PHPExcel_IOFactory::createWriter($doc, 'Excel5');
         $writer->save($xlsPath);
@@ -56,15 +62,17 @@ class CSVDownloader
         return $xlsPath;
     }
 
-    public function downloadCSV( $csvPath ){
+    public function downloadCSV($csvPath)
+    {
         $this->download($csvPath, 'csv', 'text/csv');
         @unlink($csvPath);
     }
 
-    private function download($path, $extension, $mime){
+    private function download($path, $extension, $mime)
+    {
         header('Content-Disposition: attachment; filename=oscar-export.' . $extension);
         header('Content-Length: ' . filesize($path));
-        header('Content-type: '.$mime);
+        header('Content-type: ' . $mime);
         echo file_get_contents($path);
     }
 }
