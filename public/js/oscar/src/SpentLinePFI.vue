6eba310bddd2924a1663b03173cc0304d5483d80
Stéphane Bouvry <stephane.bouvry@unicaen.fr>
2020-10-15 17:01:40 +0200

    Fix : Erreur JS lors de l'accès au détail des dépenses

--- public/js/oscar/src/SpentLinePFI.vue
+++ public/js/oscar/src/SpentLinePFI.vue
@@ -60,102 +60,47 @@
             </div>
         </div>
 
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="table table-condensed card">
+                        <tbody>
+                            <tr v-for="dt,key in byMasse.datas" v-if="key != 'recettes' && key != 'N.B'">
+                                <th>{{masses[key]}}</th>
+                                <td style="text-align: right">{{ byMasse.totaux[key].toFixed(2) }}</td>
+                            </tr>
+                            <tr>
+                                <th>Hors-masse</th>
+                                <td style="text-align: right">{{ byMasse.totaux['N.B'].toFixed(2) }}</td>
+                            </tr>
 
-        <table class="list table table-condensed table-bordered table-condensed card" v-if="spentlines != null && state == 'pack'">
-            <thead>
-            <tr>
-                <th>N°</th>
-                <th>Ligne(s)</th>
-                <th>Type</th>
-                <th>Description</th>
-                <th>Montant</th>
-                <th>Compte</th>
-                <th>Date comptable</th>
-                <th>Date paiement</th>
-                <th>Année</th>
-            </tr>
-            </thead>
-            <tbody v-for="l in spentlines">
+                            <tr>
+                                <th>Recettes</th>
+                                <td style="text-align: right">{{ byMasse.totaux['recettes'].toFixed(2) }}</td>
+                            </tr>
+                        </tbody>
+                    </div>
 
-
-            <tr>
-                <td>{{ l.refPiece }}</td>
-                <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
-                <td>{{ l.types ? l.types.join(',') : '' }}</td>
-                <td>{{ l.text.join(', ') }}</td>
-                <td style="text-align: right">{{ l.montant.toFixed(2) }}</td>
-                <td>{{ l.compteBudgetaire.join(', ') }}</td>
-                <td>{{ l.datecomptable }}</td>
-                <td>{{ l.datepaiement }}</td>
-                <td>{{ l.annee }}</td>
-            </tr>
-            </tbody>
-        </table>
-
-        <div v-else-if="state == 'masse' && spentlines != null">
-
-            <!-- Données HORS-MASSE -->
-            <div v-if="byMasse.datas['N.B'].length">
-                <h2>Dépenses Hors-Masse</h2>
-                <table class="list table table-condensed table-bordered table-condensed card">
-                    <thead>
-                    <tr>
-                        <th>N°</th>
-                        <th>Ligne(s)</th>
-                        <th>Type</th>
-                        <th>Description</th>
-                        <th style="width: 8%">Montant</th>
-                        <th style="width: 8%">Compte</th>
-                        <th style="width: 8%">Date comptable</th>
-                        <th style="width: 8%">Date paiement</th>
-                        <th style="width: 8%">Année</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr v-for="l in byMasse.datas['N.B']">
-                        <td>{{ l.refPiece }}</td>
-                        <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
-                        <td>{{ l.types ? l.types.join(',') : '' }}</td>
-                        <td>{{ l.text.join(', ') }}</td>
-                        <td style="text-align: right">{{ l.montant.toFixed(2) }}</td>
-                        <td>{{ l.compteBudgetaire.join(', ') }}</td>
-                        <td>{{ l.datecomptable }}</td>
-                        <td>{{ l.datepaiement }}</td>
-                        <td>{{ l.annee }}</td>
-                    </tr>
-                    </tbody>
-                    <tfoot>
-                    <tr style="font-weight: bold; font-size: 1.2em">
-                        <td colspan="4" style="text-align: right">Total : </td>
-                        <td style="text-align: right">{{ byMasse.totaux['N.B'].toFixed(2) }}</td>
-                        <td colspan="2">&nbsp;</td>
-                    </tr>
-                    </tfoot>
-                </table>
-                <div class="alert alert-info">
-                    Les comptes des dépenses ci-dessus ne sont pas qualifiés sur les masses attendues.
                 </div>
-            </div>
-
-            <!-- MASSE DISPONIBLES -->
-            <div v-for="masse, k in masses">
-                <h2>{{ masse }}</h2>
-                <table class="list table table-condensed table-bordered table-condensed card" v-if="byMasse.datas[k].length > 0">
-                    <thead>
+                <div class="col-md-10">
+                    <table class="list table table-condensed table-bordered table-condensed card" v-if="spentlines != null && state == 'pack'">
+                        <thead>
                         <tr>
                             <th>N°</th>
                             <th>Ligne(s)</th>
                             <th>Type</th>
                             <th>Description</th>
-                            <th style="width: 8%">Montant</th>
-                            <th style="width: 8%">Compte</th>
-                            <th style="width: 8%">Date comptable</th>
-                            <th style="width: 8%">Date paiement</th>
-                            <th style="width: 8%">Année</th>
+                            <th>Montant</th>
+                            <th>Compte</th>
+                            <th>Date comptable</th>
+                            <th>Date paiement</th>
+                            <th>Année</th>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="l in byMasse.datas[k]">
+                        </thead>
+                        <tbody v-for="l in spentlines">
+
+
+                        <tr>
                             <td>{{ l.refPiece }}</td>
                             <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
                             <td>{{ l.types ? l.types.join(',') : '' }}</td>
@@ -166,17 +111,136 @@
                             <td>{{ l.datepaiement }}</td>
                             <td>{{ l.annee }}</td>
                         </tr>
-                    </tbody>
-                    <tfoot>
-                        <tr style="font-weight: bold; font-size: 1.2em">
-                            <td colspan="4" style="text-align: right">Total : </td>
-                            <td style="text-align: right">{{ byMasse.totaux[k].toFixed(2) }}</td>
-                            <td colspan="2">&nbsp;</td>
-                        </tr>
-                    </tfoot>
-                </table>
-                <div v-else class="alert alert-info">
-                    Aucune entrée.
+                        </tbody>
+                    </table>
+                    <div v-else-if="state == 'masse' && spentlines != null">
+
+                        <!-- Données HORS-MASSE -->
+                        <div v-if="byMasse.datas['N.B'].length">
+                            <h2>Dépenses Hors-Masse</h2>
+                            <table class="list table table-condensed table-bordered table-condensed card">
+                                <thead>
+                                <tr>
+                                    <th>N°</th>
+                                    <th>Ligne(s)</th>
+                                    <th>Type</th>
+                                    <th>Description</th>
+                                    <th style="width: 8%">Montant</th>
+                                    <th style="width: 8%">Compte</th>
+                                    <th style="width: 8%">Date comptable</th>
+                                    <th style="width: 8%">Date paiement</th>
+                                    <th style="width: 8%">Année</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="l in byMasse.datas['N.B']">
+                                    <td>{{ l.refPiece }}</td>
+                                    <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
+                                    <td>{{ l.types ? l.types.join(',') : '' }}</td>
+                                    <td>{{ l.text.join(', ') }}</td>
+                                    <td style="text-align: right">{{ l.montant.toFixed(2) }}</td>
+                                    <td>{{ l.compteBudgetaire.join(', ') }}</td>
+                                    <td>{{ l.datecomptable }}</td>
+                                    <td>{{ l.datepaiement }}</td>
+                                    <td>{{ l.annee }}</td>
+                                </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr style="font-weight: bold; font-size: 1.2em">
+                                    <td colspan="4" style="text-align: right">Total : </td>
+                                    <td style="text-align: right">{{ byMasse.totaux['N.B'].toFixed(2) }}</td>
+                                    <td colspan="2">&nbsp;</td>
+                                </tr>
+                                </tfoot>
+                            </table>
+                            <div class="alert alert-info">
+                                Les comptes des dépenses ci-dessus ne sont pas qualifiés sur les masses attendues.
+                            </div>
+                        </div>
+
+                        <!-- MASSE DISPONIBLES -->
+                        <div v-for="masse, k in masses">
+                            <h2>{{ masse }}</h2>
+                            <table class="list table table-condensed table-bordered table-condensed card" v-if="byMasse.datas[k].length > 0">
+                                <thead>
+                                    <tr>
+                                        <th>N°</th>
+                                        <th>Ligne(s)</th>
+                                        <th>Type</th>
+                                        <th>Description</th>
+                                        <th style="width: 8%">Montant</th>
+                                        <th style="width: 8%">Compte</th>
+                                        <th style="width: 8%">Date comptable</th>
+                                        <th style="width: 8%">Date paiement</th>
+                                        <th style="width: 8%">Année</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="l in byMasse.datas[k]">
+                                        <td>{{ l.refPiece }}</td>
+                                        <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
+                                        <td>{{ l.types ? l.types.join(',') : '' }}</td>
+                                        <td>{{ l.text.join(', ') }}</td>
+                                        <td style="text-align: right">{{ l.montant.toFixed(2) }}</td>
+                                        <td>{{ l.compteBudgetaire.join(', ') }}</td>
+                                        <td>{{ l.datecomptable }}</td>
+                                        <td>{{ l.datepaiement }}</td>
+                                        <td>{{ l.annee }}</td>
+                                    </tr>
+                                </tbody>
+                                <tfoot>
+                                    <tr style="font-weight: bold; font-size: 1.2em">
+                                        <td colspan="4" style="text-align: right">Total : </td>
+                                        <td style="text-align: right">{{ byMasse.totaux[k].toFixed(2) }}</td>
+                                        <td colspan="2">&nbsp;</td>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                            <div v-else class="alert alert-info">
+                                Aucune entrée.
+                            </div>
+                        </div>
+
+                        <!-- Données HORS-MASSE -->
+                        <div v-if="byMasse.datas['recettes'].length">
+                            <h2>Recettes</h2>
+                            <table class="list table table-condensed table-bordered table-condensed card">
+                                <thead>
+                                <tr>
+                                    <th>N°</th>
+                                    <th>Ligne(s)</th>
+                                    <th>Type</th>
+                                    <th>Description</th>
+                                    <th style="width: 8%">Montant</th>
+                                    <th style="width: 8%">Compte</th>
+                                    <th style="width: 8%">Date comptable</th>
+                                    <th style="width: 8%">Date paiement</th>
+                                    <th style="width: 8%">Année</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr v-for="l in byMasse.datas['recettes']">
+                                    <td>{{ l.refPiece }}</td>
+                                    <td><button @click="details = l" class="btn btn-default">{{ l.details.length }}</button></td>
+                                    <td>{{ l.types ? l.types.join(',') : '' }}</td>
+                                    <td>{{ l.text.join(', ') }}</td>
+                                    <td style="text-align: right">{{ l.montant.toFixed(2) }}</td>
+                                    <td>{{ l.compteBudgetaire.join(', ') }}</td>
+                                    <td>{{ l.datecomptable }}</td>
+                                    <td>{{ l.datepaiement }}</td>
+                                    <td>{{ l.annee }}</td>
+                                </tr>
+                                </tbody>
+                                <tfoot>
+                                <tr style="font-weight: bold; font-size: 1.2em">
+                                    <td colspan="4" style="text-align: right">Total : </td>
+                                    <td style="text-align: right">{{ byMasse.totaux['recettes'].toFixed(2) }}</td>
+                                    <td colspan="2">&nbsp;</td>
+                                </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -204,10 +268,12 @@
             byMasse(){
                 let out = {
                     datas: {
-                        'N.B': []
+                        'N.B': [],
+                        'recettes': []
                     },
                     totaux: {
-                        'N.B': 0.0
+                        'N.B': 0.0,
+                        'recettes': 0.0
                     }
                 };
 
@@ -221,11 +287,17 @@
 
                     for( let indexAnnexe in groupedLine.masse ){
                         let annexe = groupedLine.masse[indexAnnexe];
+                        console.log(annexe);
                         if( !annexe )
                             annexe = 'N.B';
 
-                        out.datas[annexe].push(groupedLine);
-                        out.totaux[annexe] += groupedLine.montant;
+                        if( annexe == '1' ){
+                            out.datas['recettes'].push(groupedLine);
+                            out.totaux.recettes += groupedLine.montant;
+                        } else {
+                            out.datas[annexe].push(groupedLine);
+                            out.totaux[annexe] += groupedLine.montant;
+                        }
                     }
                 }
 
