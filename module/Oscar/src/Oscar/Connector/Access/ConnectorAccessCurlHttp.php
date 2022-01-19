f6949cb5e6a2c243a457a8b5fdddb711d58da262
Stéphane B <stephane.bouvry@unicaen.fr>
2022-01-19 14:51:19 +0100

    - FIX Curl Error 18 Transfer closed...  - Ajout d'information complémentaires en cas d'erreur liée à CURL

--- module/Oscar/src/Oscar/Connector/Access/ConnectorAccessCurlHttp.php
+++ module/Oscar/src/Oscar/Connector/Access/ConnectorAccessCurlHttp.php
@@ -28,17 +28,29 @@ class ConnectorAccessCurlHttp implements IConnectorAccess
         $this->connector = $connector;
     }
 
-    public function getDatas( $url )
+    public function getDatas($url)
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($curl, CURLOPT_COOKIESESSION, true);
 
+        // Affichage des erreurs CURL
+        curl_setopt($curl, CURLOPT_FAILONERROR, true);
+
+        // Fix CURL error curl: (18) transfer closed with 141 bytes remaining to read
+        curl_setopt($curl, CURLOPT_IGNORE_CONTENT_LENGTH, true);
+
         $return = curl_exec($curl);
 
-        if( false === $return ){
-            throw new ConnectorException(sprintf("ConnectorAccessCurlHttp(%s) n'a pas fournis les données attendues", $url));
+        if (false === $return) {
+            throw new ConnectorException(
+                sprintf(
+                    "ConnectorAccessCurlHttp(%s) n'a pas fournis les données attendues : %s",
+                    $url,
+                    curl_error($curl)
+                )
+            );
         }
 
         return PhpPolyfill::jsonDecode($return);
