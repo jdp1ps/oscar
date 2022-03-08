387ae8a6922fbeb366e6e3a915e3ffd5488ca058
Stéphane B <stephane.bouvry@unicaen.fr>
2022-03-08 18:06:03 +0100

    Ajout d'une "pause" dans l'execution de la MAJ de la BDD afin d'être sûr que Postgresql est UP

--- dockerize/skeleton/oscar/startup.sh
+++ dockerize/skeleton/oscar/startup.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
-echo "COMPOSER INSTALL"
 cd /opt/OscarApp
 
 # Composer
 composer install
 
-# DOCTRINE (MAJ Schema)
-php vendor/bin/doctrine-module orm:schema-tool:update --force
-
 # PATH Access
 chmod -R 777 data/DoctrineORMModule
+
+until PGPASSWORD="azerty" psql -h "£CONTAINER_POSTGRESQL" -U "oscar" "oscar_dev" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+# DOCTRINE (MAJ Schema)
+php vendor/bin/doctrine-module orm:schema-tool:update --force
 
 ### DEMO DATAS
 php bin/oscar.php auth:sync install/demo/authentification.json
 php bin/oscar.php organizations:sync-json install/demo/organizations.json
 php bin/oscar.php persons:sync-json install/demo/persons.json
+php bin/oscar.php activity:import-json -f install/demo/activity.json
 php bin/oscar.php check:privileges -n
 php bin/oscar.php check:privileges -n
 
