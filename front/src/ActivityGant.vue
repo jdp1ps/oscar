de2f4df9c69404f76cb14cdff473da608eda0eb1
Stéphane B <stephane.bouvry@unicaen.fr>
2022-05-25 16:44:13 +0200

    Fix début/fin

--- front/src/ActivityGant.vue
+++ front/src/ActivityGant.vue
@@ -6,18 +6,37 @@
 
     <div class="bordered-area">
       <div class="render" style="position: relative; background: rgba(255,255,255,.7)"
-           :style="{height: (50 + 80*activities.length) + 'px', width: max - min + 'px'}">
-        <header style="background: white" :style="{  }" class="header-year">
-          &nbsp;
-        </header>
+           :style="{height: (50 + 80*activities.length)*dayWidth + 'px', width: max - min + 'px'}">
 
-        <div v-for="year in yearheader" :style="{left: year.left+'px'}" class="header-year-div">
+        <div v-for="year in yearheader" :style="{left: (dayWidth * year.left)+'px'}" class="header-year-div">
           {{ year.label }}
+          <div class="months">
+            <span>Jan</span>
+            <span>Fev</span>
+            <span>Mar</span>
+            <span>Avr</span>
+            <span>Mai</span>
+            <span>Jui</span>
+            <span>Jul</span>
+            <span>Aou</span>
+            <span>Sep</span>
+            <span>Oct</span>
+            <span>Nov</span>
+            <span>Dec</span>
+          </div>
         </div>
 
 
-        <div v-for="(a, i) in activities" class="activity"
-             :style="{'top': 80 + (75*i)+'px', width: dayWidth*a.width+'px', left: a.left+'px'}">
+        <div v-for="(a, i) in activities" class="activity" style=""
+             :style="{
+                'top': 80 + (75*i)+'px',
+                width: a.width ? dayWidth*a.width+'px' : 'auto',
+                right: a.right !== false ? a.right : 'auto',
+                'border-left-style': a.start == '' ? 'dotted' : 'solid',
+                'border-right-style': a.end == '' ? 'dotted' : 'solid',
+                left: a.left+'px'
+            }"
+        >
           <div class="activity-label">
             <i class="icon-cube"></i>
             <strong class="acronym">
@@ -28,7 +47,18 @@
             </em>
             <small v-if="a.type">({{ a.type }})</small>
           </div>
-          <div class="milestone" v-for="m in a.milestones" :style="{left: m.left+'px', bottom: 0}">
+          <div class="date-start-end">
+            <small class="date-start" v-if="a.start">
+              <i class="icon-angle-left"></i>
+              {{ a.start | datation }}
+            </small>
+            <small class="date-end" v-if="a.end">
+              {{ a.end | datation }}
+              <i class="icon-angle-right"></i>
+            </small>
+          </div>
+
+          <div class="milestone" v-for="m in a.milestones" :style="{left: (m.left*dayWidth)+'px', bottom: 0}">
             <i class="icon-calendar"></i>
             <strong>
               {{ m.label }}
@@ -78,7 +108,7 @@ export default {
       let start = parseInt(this.min_date.substring(0, 4));
       let end = parseInt(this.max_date.substring(0, 4));
       let left = 0;
-      for (let i = start; i < end; i++) {
+      for (let i = start; i <= end; i++) {
         if (i != start) {
           let d = (new Date(i + '-01-01')).getTime() / 1000 / 60 / 60 / 24;
           left = d - this.min;
@@ -86,6 +116,10 @@ export default {
         out.push({label: i, left: left});
       }
       return out;
+    },
+    todayLeft(){
+      let d = (new Date().getTime() / 1000 / 60 / 60 / 24);
+      return d - this.min;
     }
   },
 
@@ -110,8 +144,18 @@ export default {
           let start = activity.start_time - this.min;
           let end = activity.end_time - this.min;
 
-          activity.left = start;
+          activity.left = activity.start_time ? start : 0;
           activity.width = (end - start);
+          activity.right = false;
+
+          if( !activity.end ){
+            console.log("PAS DE FIN DEFINI");
+            activity.right = 0;
+          }
+          if( !activity.start ){
+            activity.left = 0;
+          }
+
 
           activity.milestones.forEach(milestone => {
             milestone.left = milestone.date_time - this.min - start;
