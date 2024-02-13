83907ef4e04f9453e2280e5157ebad7bb0a81dc0
Sisomolida HING <sisomolida.hing@gmail.com>
2024-02-13 15:32:59 +0100

    debut tests unitaires

---
+++ tests/Oscar/Connector/ConnectorLdapPersonTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ConnectorLdapPersonTest extends TestCase
+{
+    public function testNoFile(){
+        $connecteurLdapPerson = new \Oscar\Connector\ConnectorLdapPersonJson();
+
+        try {
+            $connecteurLdapPerson->getFileConfig();
+            $this->fail("Fichier de configuration absent, devrait lever une exception");
+        }
+        catch (\Oscar\Exception\OscarException $e ){
+            $this->assertEquals(true, true);
+        }
+    }
+
+    public function testEmptyFile(){
+        $connecteurLdapPerson = new \Oscar\Connector\ConnectorLdapPersonJson();
+
+        try {
+            $datas = $connecteurLdapPerson->getFileConfig();
+            $this->assertNull($datas);
+        }
+        catch (\Oscar\Exception\OscarException $e ){
+            $this->fail("Les fichiers vides doivent retourner NULL");
+        }
+    }
+
+    public function testNotJsonFile(){
+        $connecteurLdapPerson = new \Oscar\Connector\ConnectorLdapPersonJson();
+
+        try {
+            $datas = $connecteurLdapPerson->getFileConfig();
+            $this->fail("Non-YAML file must throw exception !");
+        }
+        catch (\Oscar\Connector\NotYamlFileException $e ){
+            $this->assertTrue(true);
+        }
+    }
+}
