afd2bcdea1518d13c62c9387ff15b64c5d3c4bd5
Sisomolida HING <sisomolida.hing@gmail.com>
2024-02-19 21:46:32 +0100

    factorisation + sonar lint + tests unitaires avec mock

--- module/Oscar/src/Oscar/Entity/OrganizationPerson.php
+++ module/Oscar/src/Oscar/Entity/OrganizationPerson.php
@@ -26,7 +26,7 @@ class OrganizationPerson implements ILoggable
 
     /**
      * @var Organization
-     * @ORM\ManyToOne(targetEntity="Organization", inversedBy="Person")
+     * @ORM\ManyToOne(targetEntity="Organization", inversedBy="persons")
      */
     private $organization;
 
