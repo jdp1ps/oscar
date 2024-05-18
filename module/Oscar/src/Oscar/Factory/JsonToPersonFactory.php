4965db84e5edd6001ae18b327bea1a1b6d303b41
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-05-18 17:34:43 +0200

    Remove S.Hing code

--- module/Oscar/src/Oscar/Factory/JsonToPersonFactory.php
+++ module/Oscar/src/Oscar/Factory/JsonToPersonFactory.php
@@ -82,9 +82,6 @@ class JsonToPersonFactory extends JsonToObject implements IJsonToPerson
         if (property_exists($jsonData, 'mail')) {
             $object->setEmail($jsonData->mail);
         }
-        if (property_exists($jsonData, 'organizations')) {
-            $object->setOrganizations($jsonData->organizations);
-        }
 
 
         if ($connectorName !== null) {
