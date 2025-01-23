f81b0cd0d70e5ebdcd35f1c32dbf8c4a7df0ddca
Stéphane B <stephane.bouvry@unicaen.fr>
2025-01-23 11:10:32 +0100

    Compilation JS

--- public/js/oscar/vite/dist/assets/activityspentsynthesis-37e67a21.js
+++
@@ -1,1 +0,0 @@
-import{z as r}from"../vendor.js";import{A as m}from"../vendor11.js";import{m as n}from"../vendor4.js";import"../vendor9.js";let e=document.querySelector("#depenses2");const t=r(m,{url:e.dataset.url,syncurl:e.dataset.syncurl});t.config.globalProperties.$filters={money:function(o){return n.money(o)}};t.mount("#depenses2");
