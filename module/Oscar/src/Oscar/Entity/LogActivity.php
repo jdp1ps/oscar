5e4ae97bd1a4ff63f2699afad057d85086a05570
Stéphane B <stephane.bouvry@unicaen.fr>
2025-03-14 10:31:29 +0100

    Fix log warning : Undefined method "getUserActivity". The method name must start with either findBy, findOneBy or countBy!

--- module/Oscar/src/Oscar/Entity/LogActivity.php
+++ module/Oscar/src/Oscar/Entity/LogActivity.php
@@ -12,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Class Project
- * @ORM\Entity(repositoryClass="LogActivityRepository")
+ * @ORM\Entity(repositoryClass="ActivityLogRepository")
  */
 class LogActivity
 {
