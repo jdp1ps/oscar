59673afe7ec0f53d622b09b78d1aa2f677137ac9
Jean-Baptiste Oellers <jean-baptiste.oellers@unicaen.fr>
2025-05-07 11:41:10 +0200

    Ajout description du projet dans documents générés (redmine 61737)

--- module/Oscar/src/Oscar/Entity/Activity.php
+++ module/Oscar/src/Oscar/Entity/Activity.php
@@ -2368,6 +2368,7 @@ class Activity implements ResourceInterface
         // Info Projet
         $datas['project-label'] = $this->getProject() ? $this->getProject()->getLabel() : '';
         $datas['project-acronym'] = $this->getProject() ? $this->getProject()->getAcronym() : '';
+        $datas['project-description'] = $this->getProject() ? $this->getProject()->getDescription() : '';
 
         $datas['type'] = (string)$this->getActivityType();
 
